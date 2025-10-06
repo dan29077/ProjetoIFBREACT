03eede67741200762f19eff9d07ecca289f069f7
dan29077 <danielsantos.ds33@gmail.com>
2025-10-06 11:20:13 -0300

    Atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -15,3 +15,5 @@ function App() {
 }
 
 export default App
+
+//Baseado no link do projeto que te enviei me ajude á Construir duas telas: Cadastro de pessoa física e cadastro de pessoa jurídica. Colocar todos os campos previstos nos objetos PF e PJ ( em cada tela ) ex: nome, e-mail, CPF ou CNPJ, endereço (...), telefone (...), título ou inscrição estadual. sugestão : criar componentes reutilizáveis de endereço e telefones, etc
